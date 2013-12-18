c28a99ece895cda8d9dae6610080e9e310853214
aniksa <aniksa@gmail.com>
2013-12-18 17:38:50 +0200

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by anna on 12/18/13.
+//  Copyright (c) 2013 anna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Matchismo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by anna on 12/18/13.
+//  Copyright (c) 2013 anna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
